7736e1f716c6118c7a32b74c4f77fdf3f55f58a3
Alvi Choirudin <alvichoirudin652@gmail.com>
2025-07-25 23:36:30 +0700

    tugas EISD

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from 'react';
+import ProductList from './component/ProductList';
 
 function App() {
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+  fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => setProducts(data));
+  }, []);
+
+  const processedProducts = products
+  .map(product => ({
+  ...product,
+  price: product.price * 0.2
+  }))
+  .filter(product => product.rating.rate > 4.0);
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>FakeStore Products</h1>
+      <ProductList products={processedProducts} /> 
     </div>
   );
 }
---
+++ src/component/ProductCard.js
@@ -0,0 +1,16 @@
+function ProductCard({ product }) {
+  return (
+    <div className="product-card">
+      <img
+                src={product.image}
+                alt={product.title}
+                className="product-image"
+            />
+      <h3>{product.title}</h3>
+      <p>Price: ${product.price.toFixed(2)}</p>
+      <p>Rating: {product.rating.rate}</p>
+    </div>
+  );
+}
+
+export default ProductCard;
